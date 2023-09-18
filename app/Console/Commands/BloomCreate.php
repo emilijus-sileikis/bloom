037218f1cccd37365cd726f9875fe6fcfc16a0f2
Emilijus <emilijus.sileikis@mif.stud.vu.lt>
2023-09-18 11:11:25 +0300

    Create/Delete

---
+++ app/Console/Commands/BloomCreate.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+
+class BloomCreate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bloom:create {name : Class (singular), e.g. Post}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates CRUD';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+
+        if ($name)
+        {
+            $this->bloomCreate($name);
+        } else
+        {
+            $this->error("No name provided.");
+        }
+    }
+
+    protected function bloomCreate($name)
+    {
+        $this->controller($name);
+        $this->model($name);
+        $this->request($name);
+
+        File::append(base_path('routes/api.php'), "\n".'Route::resource(\'' . Str::plural(strtolower($name)) . "', '{$name}Controller');");
+
+        Artisan::call('make:migration create_' . strtolower(Str::plural($name)) . '_table --create=' . strtolower(Str::plural($name)));
+
+        $this->info($name.' CRUD created successfully.');
+    }
+
+    protected function getStub($type)
+    {
+        return file_get_contents(resource_path("stubs/$type.stub"));
+    }
+
+    protected function controller($name)
+    {
+        $template = str_replace(
+            [
+                '{{modelName}}',
+                '{{modelNameLowerCase}}',
+                '{{modelNameSingularLowerCase}}',
+            ],
+            [
+                $name,
+                strtolower(Str::plural($name)),
+                strtolower($name),
+            ],
+            $this->getStub('Controller')
+        );
+
+        file_put_contents(app_path("/Http/Controllers/{$name}Controller.php"), $template);
+    }
+
+    protected function model($name)
+    {
+        $template = str_replace(
+            ['{{modelName}}'],
+            [$name],
+            $this->getStub('Model')
+        );
+
+        file_put_contents(app_path("/{$name}.php"), $template);
+    }
+
+    protected function request($name)
+    {
+        $template = str_replace(
+            ['{{modelName}}'],
+            [$name],
+            $this->getStub('Request')
+        );
+
+        if (!file_exists($path = app_path('/Http/Requests')))
+        {
+            mkdir($path, 0755, true);
+        }
+
+        file_put_contents(app_path("/Http/Requests/{$name}Request.php"), $template);
+    }
+}
